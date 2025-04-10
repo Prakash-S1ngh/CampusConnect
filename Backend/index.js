c203a306a78b3398388f21fe2c092d7de41534f1
Prakash-S1ngh <prakashsingh14082002@gmail.com>
2025-04-10 21:28:11 +0530

    another frontend url

--- Backend/index.js
+++ Backend/index.js
@@ -17,8 +17,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// app.use(cors({
+//     origin:"http://localhost:5173",
+//     credentials:true
+// })
+// );
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://campusconnect-1-tw1a.onrender.com",
     credentials:true
 })
 );
