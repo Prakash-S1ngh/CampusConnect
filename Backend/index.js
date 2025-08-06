fe025aa8ab0e0ebae262120417a925142bd0c927
Prakash Kumar Singh <115872370+Prakash-S1ngh@users.noreply.github.com>
2025-08-07 00:55:42 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://campus-connect-d7ca.vercel.app/",
+    origin:"https://campus-connect-d7ca.vercel.app",
     credentials:true
 })
 );
